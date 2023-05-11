854e8e1fc42194b531fff4823dcd6f4b70d20a51
DWCS2023 <132108018+DWCS2023@users.noreply.github.com>
2023-05-11 12:34:24 +0800

    Inital upload.

---
+++ ChatBot.py
@@ -0,0 +1,37 @@
+Name: ⒶⒷⒸⒹⒺⒻ
+Date: 13 May 2023
+
+
+# Use the openai library
+import 📚📚📚📚📚📚
+
+# Set the API key
+openai.api_key = ⚿⚿⚿⚿⚿⚿
+
+# Gets input from user
+def getMessage():
+    theMessage = ⌨⌨⌨⌨⌨⌨('You: ')
+    return theMessage
+
+# Gets response from chatbot
+def getReply(message):
+    💾💾💾💾💾💾 = openai.Completion.create(
+        model = 'text-davinci-003',
+        prompt = message,
+    )
+    return 💾💾💾💾💾💾.choices[0].text.strip()
+
+# Repeatedly gets input and gets responses
+def main():
+    while 🚩🚩🚩🚩🚩🚩:
+        message = 📞📞📞📞📞📞
+        reply = ''
+        if ✉✉✉✉✉✉ == 👋👋👋👋👋👋:
+            print('Bot:', 'Bye!')
+            break
+        else:
+            reply = getReply(message)
+            🖶🖶🖶🖶🖶🖶('Bot:', reply)
+
+# Run the main program
+main()           
